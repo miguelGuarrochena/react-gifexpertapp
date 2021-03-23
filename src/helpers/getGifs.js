ceccd655a2fad6df97778f5a024e9e703c468e65
miguelGuarrochena <mikeguarrochena@hotmail.com>
2021-03-23 13:19:05 -0300

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async (category) => {
+  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=IBJgwEPdwG5XuJzzNMTptLYS4M6HaS7g`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    };
+  });
+
+  return gifs;
+};
